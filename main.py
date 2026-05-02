4522645bfdf6fee94cd61eccfc31dbee0562f50c
Caio Quintão <caioquintao482@gmail.com>
2026-05-02 18:08:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ usuario, computador, sistema = coletar_sistema()
 ip, internet = coletar_rede()
 
 if internet == "OK":
-    diagnostico = "Sistema funcionando normalmente ✅"
+    diagnostico = "Sistema funcionando normalmente!"
 else:
-    diagnostico = "Sem conexão com a internet ❌\nSugestão: Verifique o Wi-Fi ou cabo de rede"
+    diagnostico = "Sem conexão com a internet! \nSugestão: Verifique o Wi-Fi ou cabo de rede"
 
 print("Usuário:", usuario)
 print("Computador:", computador)
